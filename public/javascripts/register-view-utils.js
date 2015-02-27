af785bbb46c011993a8f96ed239dbc75ab0fd712
Demotive <mat@demotive.com>
2015-02-27 16:35:59 +0000

    /public/ folder shouldn't be versioned

--- public/javascripts/register-view-utils.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-  // stop the position:fixed subnav rolling into footer
-  GOVUK.stopScrollingAtFooter.addEl($('.js-stick-at-top-when-scrolling'), $('.js-stick-at-top-when-scrolling').height());
-
-  // Initiate any faux-details
-  LR.fauxDetails.init();
-
-});
