810f7482aa236a1761c93beaceddcac186a8ad12
Demotive <mat@demotive.com>
2015-06-03 12:47:10 +0100

    Thrashing out v9 journey
    * Varying journeys round the prototype begun
    * Register Summary inital layout done

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,21 @@ module.exports = {
 
     // Journey v9 routes
 
+    var v9 = {
+      buyingSummary: false,
+      boughtSummaryFirst: false,
+      signedIn: false
+    };
+
+    // v9 search page - reset everything:
+    app.get('/digital-register/journeys/v9/search-page', function(req, res) {
+      // make sure EVERYTHING is set to FALSE again
+      v9.buyingSummary = false;
+      v9.boughtSummaryFirst = false;
+      v9.signedIn = false;
+      res.render('digital-register/journeys/v9/search-page');
+    });
+
     // Carry through search terms into results pages
     app.get('/digital-register/journeys/v9/search-results', function(req, res) {
       res.render('digital-register/journeys/v9/search-results', {'terms' : req.query.s});
@@ -17,23 +32,45 @@ module.exports = {
       res.render('digital-register/journeys/v9/search-results-2', {'terms' : req.query.s});
     });
 
-    // Handle submissions from title-summary - document selection
-    app.post('/digital-register/journeys/v9/title-summary', function(req, res) {
+    // Handle submissions from access-fork - access selection
+    app.post('/digital-register/journeys/v9/access-fork', function(req, res) {
       
-      req.checkBody('documents', 'This is an error message').notEmpty();
-      var errors = req.validationErrors();  
-      
+      req.checkBody('accessType', 'This is an error message').notEmpty();
+      var errors = req.validationErrors();
+
       if(!errors) {
-        res.render('digital-register/journeys/v9/pre-sign-in');
+        if (req.body.accessType == 'summary') {
+          v9.buyingSummary = true;
+          res.render('digital-register/journeys/v9/pre-sign-in');
+        } else {
+          v9.buyingSummary = false;
+          res.redirect('digital-register/journeys/v9/select-documents');
+        }
       } else {
-        res.render('digital-register/journeys/v9/title-summary', {
+        res.render('digital-register/journeys/v9/access-fork', {
           errors: errors
         });
       }
 
     });
 
-    // Make a decision based on submission from /pre-sign-in - "Do you have an account?"
+    // Handle submissions from document selection page
+    app.post('/digital-register/journeys/v9/select-documents', function(req, res) {
+      
+      req.checkBody('documents', 'This is an error message').notEmpty();
+      var errors = req.validationErrors();
+
+      if(!errors) {
+        res.render('digital-register/journeys/v9/pre-sign-in');
+      } else {
+        res.render('digital-register/journeys/v9/select-documents', {
+          errors: errors
+        });
+      }
+
+    });
+
+    // "Do you have an account?"
     app.get('/digital-register/journeys/v9/check-sign-in', function(req, res) {
       var reg = req.query.registered;
       if (reg == 'yes') {
@@ -52,6 +89,26 @@ module.exports = {
     app.post('/digital-register/journeys/v9/account-created', function(req, res) {
       res.render('digital-register/journeys/v9/account-created');
     });
+
+    // Payment page display
+    app.get('/digital-register/journeys/v9/payment', function(req, res) {
+      // to get to payment you MUST have an account and be signed in, so
+      v9.signedIn = true;
+      res.render('digital-register/journeys/v9/payment', {
+        "buyingSummary": v9.buyingSummary
+      });
+    });
+
+    // View router after payment - summary or download?
+    app.get('/digital-register/journeys/v9/view-router', function(req, res) {
+      if (v9.buyingSummary == true) {
+        res.redirect('digital-register/journeys/v9/digital-register-view');
+      } else {
+        res.redirect('digital-register/journeys/v9/download-documents');
+      }
+    });
+
+
 
     // -------------------------------------------------------- //
 
